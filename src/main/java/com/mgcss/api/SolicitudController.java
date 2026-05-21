6528dbd2f46e1bcecb87153dfe25cf61834e2918
Madro <apgarciasanabria@gmail.com>
2026-05-21 11:07:10 +0200

    feat: añadir endpoint para crear solicitudes

--- src/main/java/com/mgcss/api/SolicitudController.java
+++ src/main/java/com/mgcss/api/SolicitudController.java
@@ -2,8 +2,13 @@ package com.mgcss.api;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import jakarta.validation.Valid;
+import com.mgcss.domain.Solicitud;
 import com.mgcss.service.ServicioSolicitud;
+
 
 @RestController
 @RequestMapping("/api/solicitudes")
@@ -18,5 +23,19 @@ public class SolicitudController {
 
         this.servicioSolicitud = servicioSolicitud;
         this.solicitudMapper = solicitudMapper;
+    }
+    
+    @PostMapping
+    public ResponseEntity<SolicitudResponseDTO> crearSolicitud(
+            @Valid @RequestBody SolicitudRequestDTO requestDTO) {
+
+        Solicitud solicitud =
+                servicioSolicitud.crearSolicitud(
+                        requestDTO.getDescripcion());
+
+        SolicitudResponseDTO response =
+                solicitudMapper.toResponseDTO(solicitud);
+
+        return ResponseEntity.ok(response);
     }
 }
